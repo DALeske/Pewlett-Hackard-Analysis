21f1fdfdbaaa48f6372567f4b5872316769d959c
David Leske <leske.david@gmail.com>
2022-02-06 16:00:20 -0600

    add queries

--- schema.sql
+++ schema.sql
@@ -42,9 +42,9 @@ CREATE TABLE dept_emp (
 );
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
+	title VARCHAR (40) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no, title)
+	PRIMARY KEY (emp_no, title, from_date)
 );
