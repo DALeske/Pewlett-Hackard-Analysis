5ad1544c44a61e3f377301fb4972ed367498dbf2
David Leske <leske.david@gmail.com>
2022-02-06 16:39:41 -0600

    SQL queries through 7.3.4

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,6 +1,5 @@
 -- MODULE 7 QUERIES
 
-
 SELECT first_name, last_name
 FROM employees
 Where birth_date BETWEEN '1952-01-01' AND '1952-12-31';
@@ -29,9 +28,44 @@ FROM employees
 Where (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
-SELECT first_name, last_name
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
 Where (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info
 
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name, 
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+
+--Joining retirment_info and dept_emp tables USING NICKNAMES
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+-- Join retirement_info and dept_emp
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp;
